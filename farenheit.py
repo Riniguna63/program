7d45f05ed094bb484930cdd4c78b636b5cbe47cd
Riniguna63 <gunasigunasi37@gmail.com>
2025-07-31 15:24:44 +0530

    Create farenheit.py
    hii

---
+++ farenheit.py
@@ -0,0 +1,7 @@
+     FAHRENHEIT
+
+step:1 start the program
+step:2 read the value of celsius
+step:3 calculate farenheit=(1.8*celsius+32)
+step:4 print the value of farenheit
+step:5 stop the program
